121ebfb4aba780b2071efae23c1d50c9cdfe4e92
mikra72 <mtkrauter@yandex.com>
2019-02-25 23:01:48 +0100

    cleanup

--- examples/metadata_example.nim
+++ examples/metadata_example.nim
@@ -13,6 +13,12 @@ proc seq2string( stringseq : var seq[string ]) : string =
     for x in stringseq:
         result = result & x
 
+proc insertCols(out_result: var seq[string], cols : var seq[string]) =         
+  for colpart in cols:
+    out_result.add(colpart)
+    out_result.add(",")
+  out_result.setLen(out_result.len-1) # strip ',' away
+
 proc doTableClone(db : nimdb_sqlite3.DbConn,
                   srctabname : string, 
                   clonetabname : string, 
@@ -30,40 +36,37 @@ proc doTableClone(db : nimdb_sqlite3.DbConn,
     var createstmt : seq[string] = @[]
     var stmtpart = "CREATE TABLE main." & clonetabname & " ( "
     createstmt.add( stmtpart )
-    for colpart in createCols:
-        createstmt.add(colpart)
-        createstmt.add(",")
-    createstmt.setLen(createstmt.len-1)
+    createstmt.insertCols(createCols)
     createstmt.add(" );")    
+
     var insertstmt : seq[string] = @[]
     var insertpart = "INSERT INTO main." & clonetabname & " ( "
     insertstmt.add(insertpart)
-    for colpart in insertcols:
-        insertstmt.add(colpart)
-        insertstmt.add(",")      
-    insertstmt.setLen(insertstmt.len-1)
+    insertstmt.insertCols(insertcols)
     insertstmt.add(")")
+ 
     var selectstmt : seq[string] = @[]
     selectstmt.add(" SELECT ") 
-    for colpart in selectcols:
-        selectstmt.add(colpart)
-        selectstmt.add(",")
-    selectstmt.setLen(selectstmt.len-1)
+    selectstmt.insertCols(selectcols)
     selectstmt.add(" FROM main.")
     selectstmt.add(srctabname)
     selectstmt.add(";") 
+ 
     var insertfromselect = concat(insertstmt,selectstmt)
    
     db.exec(SqlQuery(seq2string(createstmt)),rc)
     if not rc.evalHasError:
       db.exec(SqlQuery(seq2string(insertfromselect)),rc)
-    else:
-      echo $rc     
-
+ 
 for tn in tablenames: # browse tablenames
     var clonename = tn & "_clone"
     doTableClone(db,tn,clonename,returncode) 
     if not returncode.evalHasError:
-        echo "table " & tn & " cloned into " & clonename
+      echo "table " & tn & " cloned into " & clonename
+    else:
+      echo $returncode 
+
+let tablenamesAfterClone = allUserTableNames(db,returncode)
+echo $tablenamesAfterClone
 
 db.close(returncode)
